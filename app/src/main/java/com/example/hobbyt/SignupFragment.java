163461b2dd6dca8ebc690678ca4f0eda14deda6e
Yaroslav Obraztsov <122279615+zhjckfd135@users.noreply.github.com>
2024-05-08 13:16:50 +0300

    пиногпог

--- app/src/main/java/com/example/hobbyt/SignupFragment.java
+++ app/src/main/java/com/example/hobbyt/SignupFragment.java
@@ -2,16 +2,42 @@ package com.example.hobbyt;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class SignupFragment extends Fragment {
 
     private View view;
+    private Button signupButton;
+    private EditText editTextFirstName;
+    private EditText editTextLastName;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextPasswordConfirm;
     private TextView backText;
 
     public SignupFragment() {
@@ -20,23 +46,107 @@ public class SignupFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        view = inflater.inflate(R.layout.fragment_signup, container, false);
-        backText = (TextView) view.findViewById(R.id.textBackSignUp);
-        backText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AuthorizationActivity authorizationActivity = (AuthorizationActivity)getActivity();
-                authorizationActivity.setFragment(new LoginFragment());
-            }
-        });
+        view = initFields(inflater, container);
+        setListeners();
 
         return view;
+    }
+
+    private void setListeners() {
+        backText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { setLoginFragment(); }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { registerNewUser(); }
+        });
+    }
+
+    private View initFields(LayoutInflater inflater, ViewGroup container) {
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        backText = (TextView) view.findViewById(R.id.textBackSignUp);
+        signupButton = (Button) view.findViewById(R.id.buttonSignUp);
+        editTextFirstName = (EditText) view.findViewById(R.id.editTextFirstNameAuthorization);
+        editTextLastName = (EditText) view.findViewById(R.id.editTextLastNameAuthorization);
+        editTextEmail = (EditText) view.findViewById(R.id.editTextEmailAuthorization);
+        editTextPassword = (EditText) view.findViewById(R.id.editTextPasswordAuthorization);
+        editTextPasswordConfirm = (EditText) view.findViewById(R.id.editTextConfirmPasswordAuthorization);
+
+        return view;
+    }
+
+    public void registerNewUser(){
+        if(!checkFields()){
+            Toast.makeText(getActivity(), "Please Complete all Fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(!checkConfirmPassword()){
+            Toast.makeText(getActivity(), "Please confirm the password is correct", Toast.LENGTH_LONG).show();
+            return;
+        }
+        
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        String url = UrlBase.REGISTER_NEW_USER;
+
+        signupButton.setText("Loading...");
+        signupButton.setClickable(false);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.equalsIgnoreCase("success")){
+                    Toast.makeText(getActivity(), "Registration Successful", Toast.LENGTH_LONG).show();
+                    setLoginFragment();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                signupButton.setText("Sign Up");
+                signupButton.setClickable(true);
+                Toast.makeText(getActivity(), "Registration Un-Successful", Toast.LENGTH_LONG).show();
+                Log.w("SignUp" ,error.toString());
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String, String> params = new HashMap<>();
+                params.put("first_name", editTextFirstName.getText().toString());
+                params.put("last_name", editTextLastName.getText().toString());
+                params.put("email", editTextEmail.getText().toString());
+                params.put("password", editTextPassword.getText().toString());
+                return params;
+            }
+        };
+
+        queue.add(stringRequest);
+    }
+
+    private boolean checkFields(){
+        return !(editTextFirstName.getText().toString().isEmpty() ||
+                editTextLastName.getText().toString().isEmpty() ||
+                editTextEmail.getText().toString().isEmpty() ||
+                editTextPassword.getText().toString().isEmpty() ||
+                editTextPasswordConfirm.getText().toString().isEmpty());
+    }
+
+    private boolean checkConfirmPassword(){
+        return editTextPasswordConfirm.getText().toString().equals(editTextPassword.getText().toString());
+    }
+
+    private void setLoginFragment(){
+        AuthorizationActivity authorizationActivity = (AuthorizationActivity)getActivity();
+        authorizationActivity.setFragment(new LoginFragment());
     }
 }
---
+++ app/src/main/java/com/example/hobbyt/UrlBase.java
@@ -0,0 +1,8 @@
+package com.example.hobbyt;
+
+public final class UrlBase {
+    public final static String HOME = "http://192.168.0.104:9080/";
+    public final static String REGISTER_NEW_USER = HOME + "api/user/registerNewUser";
+    public final static String TEST = HOME + "api/test";
+    public final static String FIND_COUNT_USER_BY_EMAIL = HOME + "api/user/findCountUserByEmail";
+}
